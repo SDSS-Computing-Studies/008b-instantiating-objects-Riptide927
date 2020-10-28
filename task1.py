b150823b406fcc0245bf4b360b1835ac3f43ea5d
Peter <PeterC499@deltalearns.ca>
2020-10-28 13:15:36 -0700

    ggg

--- task1.py
+++ task1.py
@@ -73,3 +73,50 @@ Buster dog
 Shih-tzu is owned by Christy
 (10 points) 
 """
+import os
+pets=[]
+class vet:
+    animal = None
+    breed = None
+    name = None
+    owner = None
+    birthdate = None
+
+    def __init__ (self,):
+        self.name = input("What is your pets name? ")
+        self.animal = input("What is your animal? ")
+        self.breed = input("What is your animal's breed? ")
+        self.owner = input("What is your name? ")
+        self.birthdate= input("What is your animals birthdate? ")
+
+    def display(self):
+        print(self.name,self.animal)
+        print(self.breed + " is owned by " + self.owner)
+
+
+    def __del__(self):
+        pass
+
+def main():
+    while True:
+        os.system("cls")
+        choice = input("1: Enter a new pet\n2: Retrive a pet\n3: Exit\n")
+        if choice == "1":
+            pets.append( vet() )
+            
+        elif choice == "2":
+            pchoice = input("Name of pet ")
+
+            for x in range(len(pets)):
+                if pchoice == pets[x].name:
+                    pets[x].display()
+                    input()
+
+                elif len(pets) == 0:
+                    print("there are no pets here trying entering in one")
+                    break
+        else:
+            print("Fine be that way")
+            break
+
+main()
